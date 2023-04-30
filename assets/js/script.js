14e754e7571e880c306a07f9c12f06aa53a9d0a0
Jaimilit <>
2023-04-30 15:01:38 +0000

    fix errors in division section

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,10 @@ function runGame(gameType){
     else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
 
-    } else if (gameType === "divide") {
-        displayDivisonQuestion(num1, num2);
-    }
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+
+    } 
     else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting`;
@@ -94,9 +95,8 @@ return [operand1 * operand2, "multiply"];
 
 } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
-}
-else if (operator === "/") {
-        return [operand1 / operand2, "divide"];
+} else if (operator === "/") { 
+return [operand1 / operand2, "division"]; 
     }
 
 else {
@@ -144,7 +144,8 @@ document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion(operand1, operand2) {
- document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 :operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 :operand1;
-    document.getElementById('operator').textContent= "/";
+ document.getElementById("operand1").textContent = (operand1 * operand2);
+      document.getElementById("operand2").textContent = operand2;
+      document.getElementById("operator").textContent = "/";
+
 }
