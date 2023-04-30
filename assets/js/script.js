1ce24d61e3215a48e7810b7472c85497a34e4479
Jaimilit <>
2023-04-30 13:51:16 +0000

    add multiplication section

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,9 @@ function runGame(gameType){
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting`;
@@ -68,11 +71,13 @@ let operator = document.getElementById("operator").innerText;
 
 if (operator === "+") {
     return [operand1 + operand2, "addition"];
+} else if (operator === "x") {
+return [operand1 * operand2, "multiply"];
+
 } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplenented operator ${operator}. Aborting!`;
 }
-
 }
 
 /**
@@ -102,7 +107,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+document.getElementById('operand1').textContent = operand1;
+document.getElementById('operand2').textContent = operand2;
+document.getElementById('operator').textContent = "x";
 
 }
 
