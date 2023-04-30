c414dd2d3395b4925e23d6e9115801f878c43823
Jaimilit <>
2023-04-30 14:04:08 +0000

    add subtraction section

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,11 @@ function runGame(gameType){
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
 
-    } else {
+    } 
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting`;
     }
@@ -74,7 +78,11 @@ if (operator === "+") {
 } else if (operator === "x") {
 return [operand1 * operand2, "multiply"];
 
-} else {
+} else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
+}
+
+else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplenented operator ${operator}. Aborting!`;
 }
@@ -103,7 +111,10 @@ document.getElementById('operand2').textContent = operand2;
 document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 :operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 :operand1;
+    document.getElementById('operator').textContent= "-";
 
 }
 
