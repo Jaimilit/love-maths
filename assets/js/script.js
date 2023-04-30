0f3db3873e1f29f797c9e42c7936a0dd0a837728
Jaimilit <>
2023-04-30 13:24:29 +0000

    check answer function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert ("You Clicked Submit!");
+               checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -33,7 +33,24 @@ function runGame(gameType){
     }
 }
 
+
+/**
+ * checks the answer against the first element 
+ * in the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+
+let userAnswer = parseInt(document.getElementById("answer-box").value);
+let calculatedAnswer = calculateCorrectAnswer();
+let isCorrect = userAnswer === calculatedAnswer[0];
+
+if (isCorrect) {
+    alert("Hey! You got it right! :D");
+} else {
+    alert(`Awww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+}
+
+runGame(calculatedAnswer[1]);
 
 }
 
